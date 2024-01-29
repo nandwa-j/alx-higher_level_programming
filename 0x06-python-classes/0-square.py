1c14a39843df798d215ed440d8e2595b940fde69
Yukihira <Nandwa Japheth>
2024-01-29 14:56:59 +0300

    module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-  """Class Square that defines a square
+"""Class Square that defines a square"""
 
-  Attributes:
-      None
-
-      Todo: 
-          nothing
-
-  """
 
 class Square:
-    """empty class constructor
-    an instance of a square class.
-
-    Args:
-        None
-
-    Returns:
-          Nothing
-
-    """
+    """empty class constructor"""
     pass
