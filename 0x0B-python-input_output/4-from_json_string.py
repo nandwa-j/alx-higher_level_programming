92bfd805c390856e4f680c635f531e8b2d568d2b
Yukihira <Nandwa Japheth>
2024-02-06 17:01:33 +0300

    solutions for input_output tasks

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains the from_json_string function"""
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string"""
+    return json.loads(my_str)
