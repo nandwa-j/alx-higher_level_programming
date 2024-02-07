212b914d5677acaf455b1612728ca42fa64b907d
Yukihira <Nandwa Japheth>
2024-02-07 18:36:11 +0300

    corrections 4

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,59 +1,34 @@
 #!/usr/bin/python3
-""" Module to print status code """
-
+"""This script reads stdin line by line and computes metrics."""
 
 import sys
 
-
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
-
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
-
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
-
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
-
-
 if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
+    size = 0
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
     try:
+        line_count = 0
         for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
             try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
+                parts = line.split(" ")
+                file_size = int(parts[-1])
+                status_code = int(parts[-2])
+                size += file_size
+                if status_code in codes:
+                    codes[status_code] += 1
+                line_count += 1
+                if line_count % 10 == 0:
+                    print("File size: {}".format(size))
+                    for code in sorted(codes.keys()):
+                        if codes[code] != 0:
+                            print("{}: {}".format(code, codes[code]))
+            except (ValueError, IndexError):
                 pass
-            nlines += 1
     except KeyboardInterrupt:
-        magic.print_info()
+        print("File size: {}".format(size))
+        for code in sorted(codes.keys()):
+            if codes[code] != 0:
+                print("{}: {}".format(code, codes[code]))
         raise
-    magic.print_info()
+
